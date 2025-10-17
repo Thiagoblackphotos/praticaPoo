be12c56459ec77563050c36d80f3b48a10ecf040
thiago andrade nascimento <165865860+Thiagoblackphotos@users.noreply.github.com>
2025-10-17 19:56:32 -0300

    Add files via upload

---
+++ assinatura/Pessoa.java
@@ -0,0 +1,29 @@
+
+package assinatura;
+
+
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String livro;
+    private int diaDevolucao;
+    
+    public pessoa(){
+        
+    }
+    
+    public pessoa(String nome int idade){
+    
+    }
+    
+    
+    public pessoa(String nome int idade String livro){
+    
+        this.nome = nome;
+        this.idade = idade;
+        this.livro = livro;
+       
+    
+    }
+}
